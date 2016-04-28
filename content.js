d107e0cc4e6c7d4e7521a906de13cca2b6aca112
Shai Wilson <sjwilson2@usfca.edu>
2016-04-28 10:25:12 -0700

    fix dom

--- content.js
+++ content.js
@@ -8,11 +8,27 @@ for (var i = 0; i < elements.length; i++) {
 
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-            var replacedText = text.replace(/[formation]/gi, 'LEMONADE SPOILER');
-
-            if (replacedText !== text) {
-                element.replaceChild(document.createTextNode(replacedText), node);
-            }
+            // var replacedText = handleText(node);
+            handleText(node);
+            // if (replacedText !== text) {
+            //     element.replaceChild(document.createTextNode(replacedText), node);
+            // }
         }
     }
 }
+
+function handleText(textNode)
+{
+    var text = textNode.nodeValue;
+
+    text = text.replace(/hold up/gi, 'they dont love you like I love you');
+    text = text.replace(/formation/gi, 'HOLD UP');
+    text = text.replace(/world tour/gi, 'HOLD UP');
+    text = text.replace(/\blemonade\b/gi, 'HOLD UP');
+    text = text.replace(/Formation World Tour/gi, 'HOLD UP HOLD UP');
+    text = text.replace(/formation world tour/gi, 'HOLD UP');
+    text = text.replace(/Beyoncé formation tour/gi, 'HOLD UP');
+
+    textNode.nodeValue = text;
+}
+
