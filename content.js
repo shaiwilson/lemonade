fb6c540fbdea9876ab5e959dacd4e27ac973b97b
Shai Wilson <sjwilson2@usfca.edu>
2016-04-29 13:48:43 -0700

    add images

--- content.js
+++ content.js
@@ -22,10 +22,12 @@ function handleText(textNode)
     var text = textNode.nodeValue;
 
     text = text.replace(/hold up/gi, 'they dont love you like I love you ');
+    text = text.replace(/Hold Up/gi, 'they dont love you like I love you ');
     text = text.replace(/cause you/gi, 'you, you, you and me could move a mountain ');
-    text = text.replace(/sorry/gi, ', I aint sorry ');
-    text = text.replace(/\bfinal warning\b/gi, 'you know I give you life. If you try this shit again. You gon lose your wife. ');
+    text = text.replace(/sorry/gi, 'sorry? I aint sorry ');
+    text = text.replace(/final warning/gi, 'you know I give you life. If you try this shit again. You gon lose your wife. ');
     text = text.replace(/Don't Hurt Yourself/gi, 'When you hurt me, you hurt yourself ');
+    text = text.replace(/don't hurt yourself/gi, 'When you hurt me, you hurt yourself ');
     text = text.replace(/lessons/gi, 'my daddy said shoot ');
     text = text.replace(/formation/gi, 'always stay gracious, best revenge is your paper ');
     text = text.replace(/all night/gi, 'true love never has to hide ');
